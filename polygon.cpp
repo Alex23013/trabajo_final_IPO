ea38d64f0a5b75ce8d704b1c77ed746913bf5cf3
Alex23013 <alecornejolupa2013@gmail.com>
2015-12-03 09:44:36 -0500

    Update and rename Pointarray.h to polygon.cpp

---
+++ polygon.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include "pointarray.h"
+
+using namespace std;
+
+class Polygon {
+	Polygon() {PointArray points[], static int nump}
+	protected:
+		PointArray points[];
+		static int nump;
+	public:
+		double area = 0;
+		static int getNumPolygons {return nump};
+		int getNumSides {return l};
+		int getPoints {return *p};
+
+	class Rectangle {
+		Rectangle() {}
+		//2p
+		for(int i=0; i<3; i++) {
+			points[i];
+		//4int
+		for(int i=0; i<3; i++) {
+			i;
+		Polygon::area {return b*h};
+	class Triangle {
+		Triangle
+		//3p
+		for(int i=0; i<4; i++) {
+			points[i];
+		Polygon::area {return (b*h)/2};
+		
+		
+		//////////////
+		
+		
+		#include <iostream>
+#include <string>
+#include "polygon.h"
+
+using namespace std;
+
+int main() {
+	int op;
+	int r;
+	cout<<"Program\n";
+	cout<<"Que desea?\n";
+	cout<<"(1)Calcular el Area de un Rectangulo\n";
+	cout<<"(2)Calcular el Area de un Triangulo\n";
+	cin>>op;
+	if(op==1) {
+		cout<<"(1)Ingresar 2 puntos\n";
+		cout<<"(2)Ingresar 4 enteros\n";
+		if(r==1) {
+			cin>>points[2];
+			cout<<area<<endl;
+		}
+		if(r==2) {
+			for(int i=0; i<5; i++) {
+				cin>>arr[i];
+			}
+			cout<<area<<endl;			
+		}
+	if(op==2) {
+		cout<<"Ingrese 3 puntos\n";
+		cin>>points[3];
+		cout<<area<<endl;
+	}
+}
+		
