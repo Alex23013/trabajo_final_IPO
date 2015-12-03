e94e50a63b624b781c7a9738bd874f9dc23ea2a4
Alex23013 <alecornejolupa2013@gmail.com>
2015-12-03 09:39:58 -0500

    Update and rename juego.cpp to Point.h

---
+++ Point.h
@@ -0,0 +1,12 @@
+
+class Point{
+private:
+   int x,y;
+public:
+   Point();
+   Point(int _x,int _Y);
+   int getX() const;
+   int getY() const;
+   void setX(const int new_x);
+   void setY(const int new_y);
+};
