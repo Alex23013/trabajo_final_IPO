612bbad36233571aa98820115ec438eb3c194300
Alex23013 <alecornejolupa2013@gmail.com>
2015-12-03 09:54:25 -0500

    Update pila.h

--- pila.h
+++ pila.h
@@ -1,25 +1,6 @@
 using namespace std;
 
 
-////////nodo.h///////
-template<typename T>
-class nodo
-{
-public:
-	T dato;
-	nodo<T> *sig;
-	nodo<T> *ant;
-
-nodo(T dato=0)
-	{
-	this->dato=dato;
-	sig=NULL;
-	ant=NULL;
-	}
-
-//void add_dato(T dato){this->dato=dato;}
-};
-
 /////////////
 #include"nodo.h"
 template<typename T>
