f572f359f2dac6a412001b0d96b91b68bbcb0eab
Alex23013 <alecornejolupa2013@gmail.com>
2015-12-04 09:08:30 -0500

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,12 +1,23 @@
 
-class Point{
-private:
-   int x,y;
-public:
-   Point();
-   Point(int _x,int _Y);
-   int getX() const;
-   int getY() const;
-   void setX(const int new_x);
-   void setY(const int new_y);
+class Point
+{
+	public:
+		double x,y;
+	Point()
+	{
+		x=0;
+		y =0;
+	}
+	
+	Point(int _x, int _y)
+	{
+		x=_x;
+		y=_y;
+	}
+
+	int getX();
+	int getY();
+	void setX(const int new_x);
+	void setY(const int new_y);
+	
 };
